38ad38e74ee67705d13ab7148a3802fd6e51caf5
Kingoftheace <kingtheaceof@gmail.com>
2024-01-11 14:48:43 +0700

    Added a pair_by_search functionality to get token pairs.

--- dexScreener.py
+++ dexScreener.py
@@ -7,7 +7,7 @@ class Contracts:
     def __init__(self):
         self.list = {}                 # Key = token Contract / Value: Dict: Key = pair Contract, value = Pair obj
 
-    def get_pairs(self, coin_address_list: list, dicQuoteTokens: dict = None, ignore_liq_below: float = 5000, ignore_vol_below: float = 1000):
+    def get_pairs_by_base_token(self, coin_address_list: list, dicQuoteTokens: dict = None, ignore_liq_below: float = 5000, ignore_vol_below: float = 1000):
         base_url = 'https://api.dexscreener.com/latest/dex/tokens/'
         chunk_size = 1  # Maximum items per API call
         # NOTE: there seems to be an issue where not all data is retrieved if our results are too large
@@ -29,12 +29,13 @@ class Contracts:
                     pair_contract = pair_data['pairAddress'].lower()
 
                     if dicQuoteTokens is None or pair_data['quoteToken']['address'].lower() in dicQuoteTokens:
-                        if pair_data['liquidity']['usd'] > ignore_liq_below and pair_data['volume']['h24'] > ignore_vol_below:
-                            if token_contract not in self.list:
-                                self.list[token_contract] = {}
+                        if 'liquidity' in pair_data and 'volume' in pair_data:
+                            if pair_data['liquidity']['usd'] > ignore_liq_below and pair_data['volume']['h24'] > ignore_vol_below:
+                                if token_contract not in self.list:
+                                    self.list[token_contract] = {}
 
-                            pair = Pair(pair_data)
-                            self.list[token_contract][pair_contract] = pair
+                                pair = Pair(pair_data)
+                                self.list[token_contract][pair_contract] = pair
 
                 print(f'{token_contract} contract retrieved: {len(data["pairs"])} pairs')
 
@@ -46,6 +47,39 @@ class Contracts:
             else:
                 time.sleep(1)
 
+
+    def get_pairs_by_search(self, dicTokenPairs: dict, ignore_liq_below: float = 5000, ignore_vol_below: float = 1000):
+        base_url = 'https://api.dexscreener.com/latest/dex/search?q='
+        # dicTokenPairs needs to have %20 between them, the value in the dic doesn't matter
+
+        for tokenPair in dicTokenPairs.keys():
+            url = f'{base_url}{tokenPair}'
+
+            response = requests.get(url)
+            data = response.json()
+
+
+            if data['pairs'] is not None:
+                for pair_data in data['pairs']:
+                    base_contract = pair_data['baseToken']['address'].lower()
+                    quote_contract = pair_data['quoteToken']['address'].lower()
+                    pair_contract = pair_data['pairAddress'].lower()
+
+                    if f'{base_contract}%20{quote_contract}' in dicTokenPairs:
+                        if pair_data['liquidity']['usd'] > ignore_liq_below and pair_data['volume']['h24'] > ignore_vol_below:
+                            if base_contract not in self.list:
+                                self.list[base_contract] = {}
+
+                            pair = Pair(pair_data)
+                            self.list[base_contract][pair_contract] = pair
+                            print(pair_contract)
+
+                if len(dicTokenPairs) > 30:
+                    time.sleep(3)
+                elif len(dicTokenPairs) > 20:
+                    time.sleep(2)
+                else:
+                    time.sleep(1)
 
 class Pair:
 
